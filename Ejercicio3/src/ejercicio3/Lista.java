ca02e42763dab69ca880b27422b58c52ba1a4e1f
Miguel <miguelcarrizo276@gmail.com>
2023-08-11 17:42:20 -0300

    Proyecto a completar

---
+++ Ejercicio3/src/ejercicio3/Lista.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio3;
+
+/**
+ *
+ * @author Miguel
+ */
+public class Lista {
+    private String producto, categoria;
+    private double precio;
+
+    public Lista(String producto, String categoria, double precio) {
+        this.producto = producto;
+        this.categoria = categoria;
+        this.precio = precio;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Lista{" + "producto=" + producto + ", categoria=" + categoria + ", precio=" + precio + '}';
+    }
+    
+    
+}
