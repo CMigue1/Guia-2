bf0158778a5835a0a3dba9159fe9b3c182c4edf4
Miguel <miguelcarrizo276@gmail.com>
2023-08-17 16:57:44 -0300

    Nuevo Proyecto

---
+++ Ejercicio4/src/ejercicio4/Categoria.java
@@ -0,0 +1,16 @@
+package ejercicio4;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+
+/**
+ *
+ * @author Miguel
+ */
+public enum Categoria {
+    COMESTIBLE, 
+    LIMPIEZA,
+    PERFUMERIA
+}
